48e3a250e8f0291f3142e1bed8ddd3dbc27d6e5c
Lê Anh Đức <ducvui2003@gmail.com>
2024-10-13 13:56:24 +0700

    Feature/mono repo (#2)
    * refactor: setup mono repo
    
    * ref: remove driver local, use webdriver-manager
    
    * fix: add env
    
    * fix: update README
    
    * fix: handle error when get avatar

---
+++ service/extract_service/main.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from service.extract_service.src.crawler.source_1_crawler import Source1Crawler
+from service.extract_service.src.util.file_handler import write_json_to_csv
+
+
+def run_crawlers():
+    # Initialize Source1Crawler
+    source1_crawler = Source1Crawler()
+    source1_crawler.setup_driver(headless=True)  # Headless browser option
+    # source1_crawler.setJwt()
+    data = source1_crawler.crawl()
+    current_date = datetime.now().strftime("%H_%M__%d_%m_%Y")
+    filename = f"source_1_{current_date}.csv"
+    write_json_to_csv(filename, data)
+
+
+if __name__ == "__main__":
+    print("Running crawlers...")
+    run_crawlers()
---
+++ service/extract_service/src/config/setting.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv, dotenv_values
+import platform
+
+# Data config
+# Get the root directory dynamically
+ROOT_DIR = Path(__file__).parent.parent.parent
+PATH_ENV = os.path.join(ROOT_DIR, ".env")
+load_dotenv(dotenv_path=PATH_ENV)
+config = dotenv_values(PATH_ENV)
+SOURCE_A_URL = config["SOURCE_A_URL"]
+os_name = platform.system()
+DRIVER_PATH = ""
+if os_name == "Windows":
+    DRIVER_PATH = os.path.join(ROOT_DIR, "driver/chromedriver-win64/chromedriver.exe")
+elif os_name == "Linux":
+    DRIVER_PATH = os.path.join(ROOT_DIR, "driver/chromedriver-linux64/chromedriver")
+U_BLOCK = os.path.join(ROOT_DIR, "driver/chromedriver-linux64/uBlock.crx")
+
+FOLDER_DATA = os.path.join(ROOT_DIR.parent.parent, "data")
